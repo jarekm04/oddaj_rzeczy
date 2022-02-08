6e88ee9b9d447349f0ceaa1b2af973d48fdd831d
jarekm04 <jarek_m04@wp.pl>
2022-02-08 16:14:09 +0100

    three columns

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {Link as LinkScroll} from "react-scroll";
 import welcomeBg from "../assets/Home-Hero-Image.jpg";
 import decoration from "../assets/Decoration.svg";
+import ThreeColumns from "./HomeComponents/ThreeColumns";
 
 const Home = () => {
     return (
@@ -13,27 +15,28 @@ const Home = () => {
                 <section className="welcome__content">
                     <header className="header">
                         <section className="header__loginArea">
-                            <Link to="" className="loginArea__btn">Zaloguj</Link>
-                            <Link to="" className="loginArea__btn">Załóż konto</Link>
+                            <Link to="/logowanie" className="loginArea__btn">Zaloguj</Link>
+                            <Link to="/rejestracja" className="loginArea__btn">Załóż konto</Link>
                         </section>
                         <nav className="header__menu">
-                            <Link to="" className="menu__btn">Start</Link>
-                            <Link to="" className="menu__btn">O co chodzi?</Link>
-                            <Link to="" className="menu__btn">O nas</Link>
-                            <Link to="" className="menu__btn">Fundacja i organizacje</Link>
-                            <Link to="" className="menu__btn">Kontakt</Link>
+                            <LinkScroll to="" className="menu__btn">Start</LinkScroll>
+                            <LinkScroll to="" className="menu__btn">O co chodzi?</LinkScroll>
+                            <LinkScroll to="" className="menu__btn">O nas</LinkScroll>
+                            <LinkScroll to="" className="menu__btn">Fundacja i organizacje</LinkScroll>
+                            <LinkScroll to="" className="menu__btn">Kontakt</LinkScroll>
                         </nav>
                     </header>
                     <main className="main__content">
                         <h1 className="main__title">Zacznij pomagać!<br/>Oddaj niechciane rzeczy w zaufane ręce</h1>
                         <img src={decoration} alt="decoration" className="decoration"/>
                         <div className="main__btn-box">
-                            <Link to="" className="main__btn">ODDAJ<br/>RZECZY</Link>
-                            <Link to="" className="main__btn">ZORGANIZUJ ZBIÓRKĘ</Link>
+                            <Link to="/logowanie" className="main__btn">ODDAJ<br/>RZECZY</Link>
+                            <Link to="/logowanie" className="main__btn">ZORGANIZUJ ZBIÓRKĘ</Link>
                         </div>
                     </main>
                 </section>
             </div>
+            <ThreeColumns />
         </>
     );
 };
---
+++ src/components/HomeComponents/ThreeColumns.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ThreeColumns = () => {
+    return (
+        <section className="threeColumns">
+            <artcicle className="stats">
+                <h2 className="stats__num">10</h2>
+                <p className="stats__title">ODDANYCH WORKÓW</p>
+                <p className="stats__desc">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </artcicle>
+            <artcicle className="stats">
+                <h2 className="stats__num">5</h2>
+                <p className="stats__title">WSPARTYCH ORGANIZACJI</p>
+                <p className="stats__desc">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </artcicle>
+            <artcicle className="stats">
+                <h2 className="stats__num">7</h2>
+                <p className="stats__title">ZORGANIZOWANY ZBIÓREK</p>
+                <p className="stats__desc">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </artcicle>
+        </section>
+    );
+};
+
+export default ThreeColumns;
