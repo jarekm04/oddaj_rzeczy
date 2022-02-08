3dd97e0beb84ac40f8407c754b88ed5e6fcffdd8
jarekm04 <jarek_m04@wp.pl>
2022-02-08 13:26:23 +0100

    components structure

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <div className="welcome"></div>
+            <img className="welcome__bg" src="" alt="stuff"/>
+            <section className="welcome__content"></section>
+        </>
+    );
+};
+
+export default Home;
