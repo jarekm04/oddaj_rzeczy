645e42630e1df60898619d7eb8dc915ab79088d1
jarekm04 <jarek_m04@wp.pl>
2022-02-23 19:22:21 +0100

    three columns mobile view

--- src/components/HomeComponents/ThreeColumns.jsx
+++ src/components/HomeComponents/ThreeColumns.jsx
@@ -15,7 +15,7 @@ const ThreeColumns = () => {
             </article>
             <article className="stats">
                 <h2 className="stats__num">7</h2>
-                <p className="stats__title">ZORGANIZOWANY ZBIÓREK</p>
+                <p className="stats__title">ZORGANIZOWANYCH ZBIÓREK</p>
                 <p className="stats__desc">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
             </article>
         </section>
