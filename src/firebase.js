800bb3601879d1645c894e906a74b8de2879c97b
Jarek <73035959+jarekm04@users.noreply.github.com>
2022-03-02 21:55:09 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDoleuxDf5LFGqgL1d9LxrVQN5kvybD9X8",
+    apiKey: "YOUR-API-KEY",
     authDomain: "oddaj-rzeczy-2f419.firebaseapp.com",
     databaseURL: "https://oddaj-rzeczy-2f419-default-rtdb.firebaseio.com",
     projectId: "oddaj-rzeczy-2f419",
     storageBucket: "oddaj-rzeczy-2f419.appspot.com",
     messagingSenderId: "656442124744",
-    appId: "YOUR-API-KEY"
+    appId: "1:656442124744:web:acfead251cc5670b8278fd"
 };
 
 const app = initializeApp(firebaseConfig);
